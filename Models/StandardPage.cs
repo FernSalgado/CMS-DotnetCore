cf6fd5625e6bdac25934b3dec24c76e817d713b0
Fernando da Silva Salgado <salgado.fernando93@gmail.com>
2020-03-02 22:40:41 -0300

    Add blog

---
+++ Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace cms.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
